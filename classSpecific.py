0e4dc896975862b7e1c2c48a08cdc35a45eae21c
Saurabh <saurabh@krystalai.com>
2018-12-12 17:46:30 +0530

    added str to key

--- classSpecific.py
+++ classSpecific.py
@@ -125,7 +125,7 @@ class FeatureExtraction:
         lines = list()
         for key, desc_list in descriptions.items():
             for desc in desc_list:
-                lines.append(key + ' ' + desc)
+                lines.append(str(key) + ' ' + desc)
         data = '\n'.join(lines)
         file = open(filename, 'w')
         file.write(data)
