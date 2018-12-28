3529ae96ac8e6bd18651a5d958371cc60cc26768
Saurabh <saurabhc183@yahoo.com>
2018-12-28 11:36:25 +0530

    updated condition

--- flickrSpecific.py
+++ flickrSpecific.py
@@ -240,22 +240,23 @@ class FeatureExtraction:
         X1, X2, y = list(), list(), list()
         # walk through each image identifier
         for key, desc_list in descriptions.items():
-            # walk through each description for the image
-            for desc in desc_list:
-                # encode the sequence
-                seq = self.tokenizer.texts_to_sequences([desc])[0]
-                # split one sequence into multiple X,y pairs
-                for i in range(1, len(seq)):
-                    # split into input and output pair
-                    in_seq, out_seq = seq[:i], seq[i]
-                    # pad input sequence
-                    in_seq = pad_sequences([in_seq], maxlen=self.max_length)[0]
-                    # encode output sequence
-                    out_seq = to_categorical([out_seq], num_classes=self.vocab_size)[0]
-                    # store
-                    X1.append(photos[key][0])
-                    X2.append(in_seq)
-                    y.append(out_seq)
+            if key != '2258277193_586949ec62':
+                # walk through each description for the image
+                for desc in desc_list:
+                    # encode the sequence
+                    seq = self.tokenizer.texts_to_sequences([desc])[0]
+                    # split one sequence into multiple X,y pairs
+                    for i in range(1, len(seq)):
+                        # split into input and output pair
+                        in_seq, out_seq = seq[:i], seq[i]
+                        # pad input sequence
+                        in_seq = pad_sequences([in_seq], maxlen=self.max_length)[0]
+                        # encode output sequence
+                        out_seq = to_categorical([out_seq], num_classes=self.vocab_size)[0]
+                        # store
+                        X1.append(photos[key][0])
+                        X2.append(in_seq)
+                        y.append(out_seq)
         return array(X1), array(X2), array(y)
 
     # define the captioning model
